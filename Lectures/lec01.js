1f139b4de306c91771397c0bc94d877c19bb975a
Mahib Nazir <mahibnazir0@gmail.com>
2025-07-30 19:59:29 +0500

    Lec-practices

---
+++ Lectures/lec01.js
@@ -0,0 +1,19 @@
+const accountID=144453
+let accountEmail="mahib@gmail.com"
+var accountPassword="12345"
+accountCIty="Jaipur"
+
+// accountID=2
+
+console.log(accountID);
+accountEmail="bdasjhb"
+accountPassword="74657"
+accountCIty="sahiwal"
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCIty);
+
+console.table([accountID,accountEmail,accountPassword,accountCIty])
+
+
