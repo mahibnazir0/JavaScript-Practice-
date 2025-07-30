cfd4bca55ea84daf07b2bd49e3faaea2763232b0
Mahib Nazir <mahibnazir0@gmail.com>
2025-07-31 02:25:18 +0500

    Lec02 added

---
+++ Lectures/lec02.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log(3+3);
+console.log("rana");
+
+//DataTypes
+
+let name="Mahib"; //String
+let age =18; //integer
+
+let isLoggedIn=true;//Boolean
+
+console.log(typeof "rana");
+
+console.log(typeof null);
+
+console.log(typeof undefined)
+
+
+
