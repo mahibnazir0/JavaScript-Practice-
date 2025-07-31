84ef12e5aeb2f661f0836c01fc174459992488f0
Mahib Nazir <mahibnazir0@gmail.com>
2025-08-01 02:11:08 +0500

    lec 04 & 05 added

---
+++ Lectures/lec04_comparisons.js
@@ -0,0 +1,19 @@
+
+//Comparison of two different DataTypes
+console.log("02">1);
+console.log("3">2);
+
+//Comparison of Null
+console.log(null>0);
+console.log(null==0);
+console.log(null<0);
+console.log(null >=0);
+
+//Strict Check (===)
+console.log("2" === 2);
+
+
+
+
+
+
---
+++ Lectures/lec05_DataTypes.js
@@ -0,0 +1,38 @@
+//Primitive DataTypes
+//Number
+const number=100;
+const decimal=12.3;
+//Boolean
+const isLoggedIn=true;
+//Null & Undefined
+const temperature=null
+let userEmail;
+
+//Symbol
+const id=Symbol('123')
+const anotherID=Symbol('123')
+
+console.log(id ===anotherID);
+
+//BigInt
+const bigNumber=6674367347n
+
+//Non-primitive DataTypes
+//Their DataType is object
+
+//Arrays
+const heroes=["Imran Khan","ALi Jinnah","Khalid"]
+
+//Objects
+
+let myObj = {
+    name: "Mahib",
+    age : 20,
+}
+
+//Functions
+
+myFunction= function(){
+    console.log("Hello World");
+    
+}
