23ce405edaabe8859aa7b61efb7bd162737a8855
Mahib Nazir <mahibnazir0@gmail.com>
2025-08-18 05:35:13 +0500

    EventHandling Improved

--- EventHandling/script.js
+++ EventHandling/script.js
@@ -1,29 +1,28 @@
-// Basic Example of EvevtListener (It changes color of text of heading when it gets clicked)
-let h1=document.querySelector("h1");
-h1.addEventListener("click",function(){
-    h1.style.color="yellow";
+
+let h1=document.querySelector("h1")
+window.addEventListener("keydown",function(dets){
+    if(dets.key === " "){
+        h1.textContent="space"
+    }
+    else{
+        h1.textContent=dets.key;
+    }
 })
 
-// Another Example of Event Listener
-let p=document.querySelector("p");
-p.addEventListener("click",function(){
-    p.style.color="green";
+// Input Button
+let btn=document.querySelector(".btn");
+let fileInp=document.querySelector("input");
+
+btn.addEventListener("click",function(){
+    fileInp.click();
 })
-// For dobleCLick
-function dblclick(){
-    p.style.color="aqua";
-}
-p.addEventListener("dblclick",dblclick);
 
-// Remove a function
-p.removeEventListener("dblclick",dblclick);
+fileInp.addEventListener("change",function(dets){
+    const file=dets.target.files[0]
+    if(file){
+        btn.textContent=file.name;
+
+    }
 
 
-// Commomn events
-/* Input */
-
-let input=document.querySelector("input")
-
-input.addEventListener("input",function(data){
-    console.log(data);
-})
+});
